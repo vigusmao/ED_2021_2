1dbf1e59e56bc17ca37d89a0655dd0ca067dfb83
vigusmao <vigusmao@gmail.com>
2021-11-24 20:27:17 -0300

    Algoritmo eficiente para a menor diferenca

--- menor_diferenca.py
+++ menor_diferenca.py
@@ -4,32 +4,57 @@ from random import randint
 from time import time
 
 
+
+## tempo de pior caso = O(n^2)
+## espaço = O(1)
 def encontrarMenorDiferenca(elementos):
 
     menorDiferenca = None
 
-
     for i in range(len(elementos)):
         x = elementos[i]  # bloco A
+
         for j in range(i+1, len(elementos)):
             # bloco B
             y = elementos[j]       # tempo constante, ou seja, O(1)
+
             diferenca = abs(x - y) # O(1)
             if menorDiferenca == None or diferenca < menorDiferenca:
                 menorDiferenca = diferenca    # O(1)
-            if diferenca == 0:
-                return 0
+            #if diferenca == 0:
+            #    return 0
             # bloco B --> tempo constante    
 
     return menorDiferenca            
         
+
+## tempo de pior caso = O(n log n)
+## espaço = O(n)
+def encontrarMenorDiferencaOrdenando(elementos):
+
+    menorDiferenca = None  # O(1)
+
+    listaOrdenada = sorted(elementos)  # O(n log n)
+
+    for i in range(len(elementos)-1):  # n-1 repetições
+        x = listaOrdenada[i]    # O(1), isto é, tempo constante
+        y = listaOrdenada[i+1]  # O(1)
+
+        diferenca = abs(x - y) # O(1)
+        if menorDiferenca == None or diferenca < menorDiferenca:
+            menorDiferenca = diferenca    # O(1)
+        #if diferenca == 0:
+        #    return 0
+
+    return menorDiferenca            
+       
         
 
 def criarLista(n):
     lista = []
     for i in range(n):
-    #    lista.append(randint(1, 10000000))
-        lista.append(i)
+        lista.append(randint(1, 100000000))
+    #    lista.append(i)
     
     
     return lista    
@@ -48,12 +73,21 @@ while True:
     if n <= 0:
         break
     lista = criarLista(n)
+    #print(lista)
     
     inicio = time()
     diferenca = encontrarMenorDiferenca(lista)
     tempo = time() - inicio
-
+    
     print(diferenca)
-    print("tempo = %.9f\n\n" % tempo)
+    print("tempo (algoritmo quadratico) = %.9f\n\n" % tempo)
+
+    inicio = time()
+    diferenca = encontrarMenorDiferencaOrdenando(lista)
+    tempo = time() - inicio
+    
+    print(diferenca)
+    print("tempo (algoritmo n log n) = %.9f\n\n" % tempo)
+
 
 print("Tchau!")
