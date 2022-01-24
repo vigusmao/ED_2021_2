1ac60ae0a15851fcb205eefb8b7e5c8fd7665474
vigusmao <vigusmao@gmail.com>
2022-01-24 14:54:02 -0300

    memoizacao

--- fatorial.py
+++ fatorial.py
@@ -8,9 +8,20 @@ def fatorial(n):
     return n * fatorial(n-1)
 
 
+def fatorial_nao_recursivo(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+
+
 sys.setrecursionlimit(2000)
 
 for i in range(1001):
-    print i, "-->", fatorial(i)
+    print i, "-->", fatorial(i), fatorial_nao_recursivo(i)
+   
+
 
 
--- fib.py
+++ fib.py
@@ -2,27 +2,56 @@
 #  n ->        0, 1, 2, 3, 4, 5,  6,  7
 
 
+import sys
+sys.setrecursionlimit(3000)
+
+# MEMOIZATION (MEMOIZAÇÃO)
+memo = {}
+
 
 def fib(n):
+    result_from_memo = memo.get(n)       # O(1)
+    if result_from_memo is not None:     # O(1)
+        return result_from_memo          # O(1)
 
     if n <= 1:
         return 1
 
-    return fib(n-1) + fib(n-2)
+    result = fib(n-1) + fib(n-2)
+
+    memo[n] = result    # anota no memo o resultado para este valor de n
+
+    return result
 
 
-for i in range(61):
+for i in range(2001):
     print i, "-->", fib(i)
 
 #   fib(40)
-#   quantas vezes fib(40)  ?   1
-#   quantas vezes fib(39)  ?   1
-#   quantas vezes fib(38)  ?   2
-#   quantas vezes fib(37)  ?   3
-#   quantas vezes fib(36)  ?   5
-#   quantas vezes fib(35)  ?   8
+#   quantas vezes fib(40)  ?   1 = fib(0)
+#   quantas vezes fib(39)  ?   1 = fib(1)
+#   quantas vezes fib(38)  ?   2 = fib(2)
+#   quantas vezes fib(37)  ?   3 = fib(3)
+#   quantas vezes fib(36)  ?   5 = fib(4)
+#   quantas vezes fib(35)  ?   8 = fib(5)
+#   ....
 #   quantas vezes fib(1)   ?   fib(39) vezes!!!!   
 
 
+######
+#
+#                                          40
+#                                      /      \ 
+#                                   39         38*
+#                                 /   \              
+#                               38      37*              
+#                             /   \           
+#                           37    36*      
+#                         /  \                 
+#                       36   35*
+#                      /  \
+#                    35   34*
+#
+#
 
 
