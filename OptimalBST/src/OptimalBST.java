53340ea357ed388ea9369ec9115ffcbbd21eda90
vigusmao <vigusmao@gmail.com>
2022-02-07 07:35:30 -0300

    arvore binaria de busca otima (PD)

--- OptimalBST/src/OptimalBST.java
+++ OptimalBST/src/OptimalBST.java
@@ -8,20 +8,101 @@ public class OptimalBST {
     Map<ComecoEFim, Integer> memoPesos;
     Map<ComecoEFim, Integer> memoIndicesRaizes;
 
+//    public int getPesoArvoreOtima(
+//            List<ChaveEFrequencia> lista) {   // Programação Dinâmica clássica (bottom-up)
+//
+//        memoPesos = new HashMap<>();
+//        memoIndicesRaizes = new HashMap<>();
+//
+//        Collections.sort(lista);
+//
+//        for (int tamanhoRange = 0; tamanhoRange < lista.size(); tamanhoRange++) {
+//
+//            for (int comeco = 0; comeco + tamanhoRange < lista.size(); comeco++) {
+//                int fim = comeco + tamanhoRange;
+//
+//                ComecoEFim comecoEFim = new ComecoEFim(comeco, fim);
+//
+//                if (fim == comeco) {
+//                    int pesoOtimo = lista.get(comeco).getFrequencia();
+//                    memoPesos.put(comecoEFim, pesoOtimo);
+//                } else {
+//                    int somaDeTodasAsFrequencias = 0;
+//                    for (int i = comeco; i <= fim; i++) {
+//                        somaDeTodasAsFrequencias += lista.get(i).getFrequencia();
+//                    }
+//
+//
+//                    /** TO DO:  falta terminar aqui!!!!!!!
+//                     *          Estamos tentando todas as raízes, mas não estamos retendo
+//                     *          a árvore de menor peso!!!!
+//                     *          Não estamos retendo a árvore de menor peso!
+//                     */
+//                    int pesoOtimo = Integer.MAX_VALUE;
+//                    Integer indiceRaizOtima = null;
+//
+//                    for (int indiceRaiz = comeco; indiceRaiz <= fim; indiceRaiz++) {
+//
+//                        int pesoOtimoAEsquerda = indiceRaiz == comeco
+//                                ? 0
+//                                : memoPesos.get(new ComecoEFim(comeco, indiceRaiz - 1));
+//                        int pesoOtimoADireita = indiceRaiz == fim
+//                                ? 0
+//                                : memoPesos.get(new ComecoEFim(indiceRaiz + 1, fim));
+//                        int pesoTotalComEssaRaiz = pesoOtimoAEsquerda + pesoOtimoADireita +
+//                                somaDeTodasAsFrequencias;
+//
+//                        if (pesoTotalComEssaRaiz < pesoOtimo) {
+//                            pesoOtimo = pesoTotalComEssaRaiz;
+//                            indiceRaizOtima = indiceRaiz;
+//                        }
+//                    }
+//
+//                    memoPesos.put(comecoEFim, pesoOtimo);
+//                    memoIndicesRaizes.put(comecoEFim, indiceRaizOtima);
+//                }
+//            }
+//        }
+//
+//        return memoPesos.get(new ComecoEFim(0, lista.size() - 1));
+//
+//        /**
+//         *       Programação Dinâmica (bottom-up)
+//         *
+//         *       Exemplo com 4 chaves
+//         *
+//         *       0..0 -->
+//         *       1..1 -->
+//         *       2..2 -->
+//         *       3..3 -->
+//         *
+//         *       0..1 -->
+//         *       1..2 -->
+//         *       2..3 -->
+//         *
+//         *       0..2 -->
+//         *       1..3 -->
+//         *
+//         *       0..3 --> [ OBJETIVO ]
+//         */
+//
+//
+//
+////        return encontrarPesoOtimo(
+////                lista, 0, lista.size() - 1);
+//    }
+
     public int getPesoArvoreOtima(
             List<ChaveEFrequencia> lista) {
 
         memoPesos = new HashMap<>();
         memoIndicesRaizes = new HashMap<>();
 
-        Collections.sort(lista);
-
-        return encontrarPesoOtimo(
-                lista, 0, lista.size() - 1);
+        return encontrarPesoOtimo(lista, 0, lista.size() - 1);  // chamada original ("top")
     }
 
     private int encontrarPesoOtimo(
-            List<ChaveEFrequencia> lista, int comeco, int fim) {
+            List<ChaveEFrequencia> lista, int comeco, int fim) {   // P.D. top-down (com memoização)
 
         if (comeco > fim) {
             return 0;  // árvore vazia
@@ -33,11 +114,11 @@ public class OptimalBST {
 
         ComecoEFim comecoEFim = new ComecoEFim(comeco, fim);
 
-        // verifica se já tem a resposta no memoPesos
-        Integer resultadoPreArmazenado = memoPesos.get(comecoEFim);
-        if (resultadoPreArmazenado != null) {
-            return resultadoPreArmazenado;
-        }
+//        // verifica se já tem a resposta no memoPesos
+//        Integer resultadoPreArmazenado = memoPesos.get(comecoEFim);
+//        if (resultadoPreArmazenado != null) {
+//            return resultadoPreArmazenado;
+//        }
 
         int somaDeTodasAsFrequencias = 0;
         for (int i = comeco; i <= fim; i++) {
@@ -63,8 +144,6 @@ public class OptimalBST {
 
         return pesoOtimo;
     }
-
-
 
 
 }
